d495fa8e6453589c385a343517e4008e6e0385df
mohsin786-sudo <kazimohsin2399@gmail.com>
2025-02-13 06:28:29 +0000

    Datatypes

---
+++ 01_Datatypes/Datattypes.js
@@ -0,0 +1,15 @@
+console.log("mohsin")
+console.log(3+3)
+let name = "mohsin"  //string
+let age = 21      // numbers
+let isLoggedIn = false   //boolean
+
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean=> true false
+// Null => standalone value
+//undefined
+console.log(typeof"mohsin")
+console.log(typeof true)
+console.log(typeof 3)
