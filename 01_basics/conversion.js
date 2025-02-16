e7f8816eb90aeacd3215f52f9f499a58fd7e2b4d
mohsin786-sudo <kazimohsin2399@gmail.com>
2025-02-16 06:34:20 +0000

    conversion.js

---
+++ 01_basics/conversion.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+console.log(typeof(score));
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log( typeof valueINumber);
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// True => ; false => 0
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
