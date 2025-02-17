3c5bf7ba5ff9af789b8ddbe27e43077d33de3530
mohsin786-sudo <kazimohsin2399@gmail.com>
2025-02-17 06:18:14 +0000

    primitive and non-primitive Datatypes.

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive 
+
+// 7 types : String , Number ,  Booolean, null,undefined, symbol 
+// Bigint
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId );
+
+const bigNumber = 123437287836468739n
+
+
+// Reference (Non prirmitive)
+
+// Array,Objects,Functions
+
+const heros = ["Shaktiman","naagraj","doga"];
+let myObj = {
+    name:"mohsin",
+    age:21,
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+
+}
+
+console.log(typeof bigNumber);
